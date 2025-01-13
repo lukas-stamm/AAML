3b4eaaccd9e69225921b9b29b2c6b14b18c62a91
lukas-stamm <lukas.stamm2002@gmail.com>
2025-01-13 10:59:12 +0100

    artist classification

---
+++ cudacheck.py
@@ -0,0 +1,7 @@
+import torch
+
+
+print("Is CUDA available:", torch.cuda.is_available())
+print("CUDA version:", torch.version.cuda)
+print("PyTorch version:", torch.__version__)
+print("GPU Name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
